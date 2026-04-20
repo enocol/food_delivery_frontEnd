6fad365936f3ab2f1c06098fb443f2a19e939316
enocol <eno_col@yahoo.com>
2026-04-20 19:58:57 +0100

    Fix bug

--- data/foodFilters.js
+++ data/foodFilters.js
@@ -17,16 +17,16 @@ export const FILTER_ALIASES = {
   "Fried Rice": ["fried rice", "friend rice"],
 };
 
-export const FOOD_FILTER_IMAGES = {
-  Achu: require("../assets/foods/achu.png"),
-  "Fried Rice": require("../assets/foods/fried-rice.png"),
-  "Corn Chaff": require("../assets/foods/corn-chaff.png"),
-  Eru: require("../assets/foods/eru.png"),
-  "Grilled Fish": require("../assets/foods/grilled-fish.png"),
-  Koki: require("../assets/foods/koki.png"),
-  Mbanga: require("../assets/foods/mbanga.png"),
-  Ndole: require("../assets/foods/ndole.png"),
-  "Pepper Soup": require("../assets/foods/pepper-soup.png"),
-  Suya: require("../assets/foods/suya.png"),
-  // "Street Food": require("../assets/foods/street-food.png"),
-};
+// export const FOOD_FILTER_IMAGES = {
+//   Achu: require("../assets/foods/achu.png"),
+//   "Fried Rice": require("../assets/foods/fried-rice.png"),
+//   "Corn Chaff": require("../assets/foods/corn-chaff.png"),
+//   Eru: require("../assets/foods/eru.png"),
+//   "Grilled Fish": require("../assets/foods/grilled-fish.png"),
+//   Koki: require("../assets/foods/koki.png"),
+//   Mbanga: require("../assets/foods/mbanga.png"),
+//   Ndole: require("../assets/foods/ndole.png"),
+//   "Pepper Soup": require("../assets/foods/pepper-soup.png"),
+//   Suya: require("../assets/foods/suya.png"),
+//   // "Street Food": require("../assets/foods/street-food.png"),
+// };
