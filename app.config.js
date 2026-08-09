c8fe4b819737d5eeed383b69f8ca5a6facc24f6d
enocol <eno_col@yahoo.com>
2026-08-09 15:44:13 +0100

    fix bug

--- app.config.js
+++ app.config.js
@@ -1,11 +1,6 @@
-const appJson = require("./app.json");
-
 module.exports = ({ config }) => {
   return {
-    ...appJson,
-    expo: {
-      ...appJson.expo,
-      newArchEnabled: true,
-    },
+    ...config,
+    newArchEnabled: true,
   };
 };
